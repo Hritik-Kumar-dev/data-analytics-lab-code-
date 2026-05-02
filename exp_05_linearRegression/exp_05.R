4d792022e2caeb7279684357ec37bf1e9afc961b
Hritik-Kumar-dev <hritikkumar.dev77@gmail.com>
2026-05-02 08:56:43 +0530

    final

--- exp_05_linearRegression/exp_05.R
+++ exp_05_linearRegression/exp_05.R
@@ -9,17 +9,22 @@ model <- lm(price ~ sqft_living, data=data)
 
 summary(model)
 
+# Create and save plot
+png("regression_plot.png", width=800, height=600)
 plot(data$sqft_living, data$price,
 main="House Price vs Living Area",
-xlab="Living Area",
-ylab="Price",
+xlab="Living Area (sqft)",
+ylab="Price ($)",
 col="blue",
 pch=19)
 
 abline(model, col="red", lwd=2)
+dev.off()
+cat("\nPlot saved to regression_plot.png\n")
 
 new_data <- data.frame(sqft_living=c(1000,1500,2000))
 
 predicted_prices <- predict(model, newdata=new_data)
 
+cat("\nPredicted Prices for given square footage:\n")
 print(predicted_prices)
